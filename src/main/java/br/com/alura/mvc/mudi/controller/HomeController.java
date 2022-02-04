c0ba2e9ec8ced675eeba5fbf8961fce9da920174
José Luan Lima da Costa <joseluan620@gmail.com>
2022-02-04 16:56:09 -0300

    include JPARepository

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -2,31 +2,38 @@ package br.com.alura.mvc.mudi.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
 
 @Controller
 public class HomeController {
 	
-	@PersistenceContext
-	private EntityManager entityManager;
-
+	//Anotação para que Spring realiza a injenção de dependências
+	
+	//Utilizamos o @AutoWired para indicar ao Spring que o objeto anotado 
+	//é um componente ou Bean dele e que queremos que ele nos dê uma instância 
+	//por meio do recurso de injeção de dependência.
+	@Autowired
+	private PedidoRepository repository;
+	
 	@GetMapping("/home")
-	public String home(Model model) {
-		//Consulta com HQL usa o nome da entidade
-		Query query = entityManager.createQuery("SELECT p FROM Pedido p", Pedido.class);
-		List<Pedido> pedidos = query.getResultList();
+	public ModelAndView home() {
 		
-		model.addAttribute("pedidos",pedidos);
+//		List<Pedido> pedidos = repository.recuperaTodosOsPedidos();
+		List<Pedido> pedidos = repository.findAll();
 		
-		return "home";
+//		model.addAttribute("pedidos", pedidos);
+//		
+//		return "home";
+		
+		ModelAndView mv = new ModelAndView("home");
+	    mv.addObject("pedidos", pedidos);
+	    return mv; 
 	}
 	
 }
