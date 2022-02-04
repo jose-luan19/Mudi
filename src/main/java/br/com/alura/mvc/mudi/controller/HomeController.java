ffcbb02d472bf196db077e6aed0dc2e8c107f2f2
José Luan Lima da Costa <joseluan620@gmail.com>
2022-02-04 14:52:44 -0300

    criando home-page e estilizando com o thymeleaf e bootstrap

---
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -0,0 +1,32 @@
+package br.com.alura.mvc.mudi.controller;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.alura.mvc.mudi.model.Pedido;
+
+@Controller
+public class HomeController {
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@GetMapping("/home")
+	public String home(Model model) {
+		//Consulta com HQL usa o nome da entidade
+		Query query = entityManager.createQuery("SELECT p FROM Pedido p", Pedido.class);
+		List<Pedido> pedidos = query.getResultList();
+		
+		model.addAttribute("pedidos",pedidos);
+		
+		return "home";
+	}
+	
+}
